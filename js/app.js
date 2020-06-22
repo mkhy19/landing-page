24b77dc97afb017332a0cc2f27c1691f8f0cbe05
mkhy19 <mkhy88519@gmail.com>
2020-06-22 08:53:37 +0200

    add the functionalities

--- js/app.js
+++ js/app.js
@@ -13,45 +13,64 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
+/**************************************************/
 
+// Define Global Variables
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+const navElements = document.querySelectorAll('section')
+const navList = document.getElementById('navbar__list')
 
+// End Global Variables
 
+/**************************************************/
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+// Begin Main Functions
 
 // Build menu 
+// create a navigation menu based on the sections of the page
+navElements.forEach(el => {
+    const navlistElement = `<li class='menu__link ${el.className}' data-link=${el.id}><a href="#${el.id}">${el.dataset.nav}</li>`
+    // add each navigation item to your menu
+    navList.insertAdjacentHTML('beforeend', navlistElement)
+  })
 
-// Scroll to section on link click
+// Build the nav
+// by clicking on a navigation item should scroll to the appropriate section of the page
+navList.addEventListener('click', e => {
+    e.preventDefault()
+    const parent = e.target.hasAttribute('data-link') ? e.target : e.target.parentElement
 
-// Set sections as active
+    // Scroll to anchor ID using scrollTO event: Scroll to section on link click
+    // functionality to scroll to sections using scrollIntoView the most simple JS methods for scrolling
+    const elementToScrollTo = document.getElementById(parent.dataset.link)
+    elementToScrollTo.scrollIntoView({behavior: "smooth", block: "end", inline: "end"})
+  })
 
+// Add class 'active' to section when near top of viewport
+const callbackfn = entries => {
+    entries.forEach(entry => {
+      const navListElement = document.querySelector(
+        `.menu__link[data-link='${entry.target.id}']`,
+      )
+      const section = document.getElementById(entry.target.id)
+  
+      // Set sections as active
+      // functionality to distinguish the section in view: to highlight active section
+      // using classList methods to change the CSS being displayed. add, remove and contains methods
+      if (entry && entry.isIntersecting) {
+        navListElement.classList.add('active')
+        section.classList.add('active')
+      }
+      
+      else {
+        if (navListElement.classList.contains('active')) {
+          navListElement.classList.remove('active')
+        }
+        if (section.classList.contains('active')) {
+          section.classList.remove('active')
+        }
+      }
+    })
+  }
 
+// End Main Functions
